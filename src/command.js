7b2aeb36df28650b4a366682574c37bd8ab8b68a
RiseofRice <xthekay@gmail.com>
2024-01-22 04:12:59 +0100

    Added some stuff

---
+++ src/command.js
@@ -0,0 +1,24 @@
+import { REST, Routes } from 'discord.js';
+
+const CLIENT_ID = '403335985875648543'
+const TOKEN = ""
+
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
