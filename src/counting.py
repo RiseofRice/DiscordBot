a9d2292595e83d93e37a647229be4104e51f8d4e
RiseofRice <xthekay@gmail.com>
2024-02-11 10:55:34 +0100

    Changed a couple of @coderabbitai's suggestions

--- src/counting.py
+++ src/counting.py
@@ -1,18 +1,19 @@
 def counting_loop(n):
-    #returns n number to inputparsing
+    
     
     while True:
         try:
-            zahl = int(input())
+            zahl = int(input("Enter a number: "))
             if zahl == n:
                 print("richtig")
                 n = n+1 
             else:
-                print("Restart")
+                print("Count has been reset. Please start from 1.")
                 n=0
-                #bot posts a sad gif
+                
         except ValueError:
-            print("Nur nummern ")
+            print("Please enter numbers only. Resetting the game. Please start from 1 again :) ")
+            n=0 
         
 
 
